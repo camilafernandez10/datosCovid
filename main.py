efe8340e74ab8d185dbbb6f4c7d3095b4eb9b5f5
camilaFernandez <maria.fernandezo1@correo.usa.edu.co>
2020-09-27 09:29:00 -0500

    conexion a base de datos y metodo leer dts

--- main.py
+++ main.py
@@ -5,20 +5,50 @@
 
 
 import requests
+import mysql.connector
+import csv
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='DatosCovid',
+                                         user='root',
+                                         password='1234')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
+
 def cargarDatos():
     url = 'https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD'
     myfile = requests.get(url)
     open('datosCovid.csv', 'wb').write(myfile.content)
-#def leerDatos():
-#    with open('datos.csv', newline='') as File:
-#        reader = csv.reader(File)
-#        for row in reader:
-#            print(row)
+def leerDatos():
+    with open('datosCovid.csv', newline='') as File:
+        reader = csv.reader(File)
+        i=0
+        for row in reader:
+            print(row)
+            if i<50:
+                i=1+i
+            else:
+                break
 cargarDatos()
-#leerDatos()
+leerDatos()
+
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    cargarDatos()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
