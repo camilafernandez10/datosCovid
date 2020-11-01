728b90e546fe91e3145884693b3b3254e27e46c1
BuildTools <santipego0001@gmail.com>
2020-10-31 20:07:33 -0500

    Se agregan datos de bogotá

--- base datos covid.sql
+++ base datos covid.sql
@@ -1,40 +1,55 @@
 create database DatosCovid;
 use DatosCovid;
-
-Create table datos(
-idCasos int not null primary key,#0
-fechaNotificacion timestamp , #1
-codigoDIVIPOLA int not null , #2
-ciudad varchar(50) , #3
-departamento varchar (70) , #4
-atencion varchar(50) ,#5
-edad int not null,#6
-sexo varchar(1) ,#7
-tipo varchar(50) , #8
-estado varchar(50) ,#9
-paisProcedencia varchar(50) ,#10
-fis timestamp ,#11
-fechaDeMuerte timestamp null, #12
-fechaDiagnostico timestamp ,#13
-fechaRecuperado timestamp null,#14
-fechaReporteWeb timestamp ,#15
-tipoRec varchar(50),#16
-codigoDepartamento int not null,#17
-codigoPais int null,#18
-etnia varchar(50),#19 
-nombreGrupoEtnico varchar(70));#20
 drop table datos;
-select * from datos;
+Create table datos(
+fecha_reporte_web timestamp,
+idCaso int not null primary key,#0 check
+fecha_de_notificacion timestamp , #1 check
+departamento varchar (70) , #3 check
+municipio varchar(70) , #2 check
+edad int not null,#5 check
+sexo varchar(1) ,#6 check
+tipoContagio varchar(50) , #7 check
+ubicacion varchar(20), #19 check 
+estado varchar(50) ,#8
+paisProcedencia varchar(50) ,#9
+recuperado varchar (11),
+fecha_inicio_sintomas timestamp ,#10
+fecha_de_muerte timestamp null, #11
+fecha_diagnostico timestamp ,#12
+fecha_de_recuperacion timestamp null,#13
+tipo_recuperacion varchar(50),#15
+nombreGrupoEtnico varchar(70)
+);
+drop table datos;
+select * from datos where fechaDiagnostico between '2020-09-01' and '2020-09-30' order by fechaDiagnostico desc;
 
-insert ignore into datos 
-values (idCasos, fechaNotificacion, codigoDIVIPOLA, ciudad, departamento,atencion,edad,sexo,tipo,estado,paisProcedencia,fis,fechaDiagnostico, fechaRecuperado,fechaReporteWeb,tipoRec,codigoDepartamento,codigoPais,etnia);
+create table datosBogota(
+id int auto_increment primary key,
+fecha_inicio_sintomas date,
+fecha_diagnostico date,
+ciudad varchar(20),
+localidad_asis varchar(50),
+edad int,
+sexo varchar(1),
+fuente_o_tipo_contagio varchar(30),
+ubicacion varchar(20),
+estado varchar(18)
+);
 
 replace into datos 
-values (idCasos, fechaNotificacion, codigoDIVIPOLA, ciudad, departamento,atencion,edad,sexo,tipo,estado,paisProcedencia,fis,fechaDiagnostico, fechaRecuperado,fechaReporteWeb,tipoRec,codigoDepartamento,codigoPais,etnia);
+values (fecha_reporte_web,idCaso, fecha_de_notificacion, departamento,municipio,edad,sexo,tipoContagio,ubicacion,estado,paisProcedencia,recuperado,fecha_inicio_sintomas,fecha_de_muerte,fecha_diagnostico,fecha_de_recuperacion,tipo_recuperacion,nombreGrupoEtnico);
 use mysql;
+
+insert into datos (fecha_inicio_sintomas,fecha_diagnostico,ciudad,localidad_asis,edad,sexo,fuente_o_tipo_contagio,ubicacion,estado)values
 create user natalia@25.56.171.88;
 grant all privileges on DatosCovid.* to natalia@25.56.171.88;
 create user santiago@25.57.195.191;
 grant all privileges on DatosCovid.* to santiago@25.57.195.191;
 create user camila@25.56.168.158;
 grant all privileges on datoscovid.*to camila@25.56.168.158;
+
+select ciudad, count(*) as contagios from datos group by ciudad order by contagios desc;
+select departamento, sexo, count(*) as contagios from datos 
+group by departamento,sexo 
+order by departamento desc, contagios asc;
